493265f2bd920747f2a088a6508874d79be0479e
EdwM20 <ed.m.2015@mail.ru>
2020-11-18 03:01:54 +0700

    добавил вчерашний файл

---
+++ app.js
@@ -0,0 +1,43 @@
+$(function() {
+	console.log("jQuery is ready!");
+
+
+function main()
+{
+	let a = 5;
+	let b = 6;
+
+	
+	console.log(a + b);
+
+	//let body = document.getElementByTegName("body");
+
+	let button = document.createElement("button");
+	button.innerHTML = "кнопка"
+
+	document.body.appendChild(button);	
+
+	$("button").on('click', function(){
+		alert('Hello, it is jQuery')	
+	});
+
+
+
+	$.ajax({
+		url: "echo.php",
+		async: true,
+		metod: "POST",
+		data: ({number: 5}),
+		dataType: "html"
+		// поменял на html потому что number 
+		}).done(function(respons) {
+			console.log ('success');
+			console.log ('respons');
+		});
+	
+
+}
+
+main();
+
+});
